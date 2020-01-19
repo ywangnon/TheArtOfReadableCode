5d10e0cd9b5d1e171d36e775a51c102a068a4b5f
ywangnon <ywangnon123@gmail.com>
2020-01-19 23:34:55 +0900

    add sample

--- Chapter1/Chapter1.playground/Contents.swift
+++ Chapter1/Chapter1.playground/Contents.swift
@@ -10,7 +10,7 @@ func sample() {
 }
 
 // 2
-func sample2() {
+func sample1_2() {
     let list = [1,2,3,4]
     if list == nil { return }
     var i = 0
@@ -23,8 +23,8 @@ func sample2() {
 
 print("sample1")
 sample()
-print("sample2")
-sample2()
+print("sample1_2")
+sample1_2()
 
 /*
  C 언어로 node를 생성하여 예제를 만들었는데 swift에서 node를 생성할 수 있나?
@@ -34,10 +34,30 @@ sample2()
  2
  3
  4
- sample2
+ sample1_2
  1
  2
  3
  4
  Fatal error: Index out of range
+ */
+
+let 거듭제곱 = Int(arc4random())
+let 가수 = Int(arc4random())
+
+func sample2_1() -> Int{
+    return 거듭제곱 >= 0 ? 가수 * (1 << 거듭제곱) : 가수 / (1 << -거듭제곱)
+}
+
+func sample2_2() -> Int {
+    if 거듭제곱 >= 0 {
+        return 가수 * (1 << 거듭제곱)
+    } else {
+        return 가수 / (1 << -거듭제곱)
+    }
+}
+
+// 위가 코드가 더 짧지만 밑이 가독성이 더 좋다.
+/*
+ 책이 정말 코드에서 필요한 부분만 있어서 변수를 따로 만들어야함
  */
